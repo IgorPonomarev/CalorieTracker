5ad74d96084d94b27c324461dae79b3199529afa
Igor Ponomarev <dviss1510@gmail.com>
2023-03-07 13:45:47 +0400

    feat: tracker overview screen - day selector

--- tracker/tracker_presentation/src/main/java/com/dviss/tracker_presentation/tracker_overview/TrackerOverviewScreen.kt
+++ tracker/tracker_presentation/src/main/java/com/dviss/tracker_presentation/tracker_overview/TrackerOverviewScreen.kt
@@ -1,7 +1,6 @@
 package com.dviss.tracker_presentation.tracker_overview
 
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -9,6 +8,7 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.dviss.core.util.UiEvent
 import com.dviss.core_ui.LocalSpacing
+import com.dviss.tracker_presentation.tracker_overview.components.DaySelector
 import com.dviss.tracker_presentation.tracker_overview.components.NutrientsHeader
 
 @Composable
@@ -26,6 +26,20 @@ fun TrackerOverviewScreen(
     ) {
         item {
             NutrientsHeader(state = state)
+            Spacer(modifier = Modifier.height(spacing.spaceMedium))
+            DaySelector(
+                date = state.date,
+                onPreviousDayClick = {
+                    viewmodel.onEvent(TrackerOverviewEvent.OnPreviousDayClick)
+                },
+                onNextDayClick = {
+                    viewmodel.onEvent(TrackerOverviewEvent.OnNextDayClick)
+                },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = spacing.spaceMedium)
+            )
+            Spacer(modifier = Modifier.height(spacing.spaceMedium))
         }
     }
 }
